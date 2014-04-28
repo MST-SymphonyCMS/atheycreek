3b498e9f76135b6147cfaf1c5066ad5ed6d82862
Jonathan Simcoe <jonathan@simko.io>
2014-04-28 14:28:49 -0700

    updates

---
+++ Gruntfile.js
@@ -0,0 +1,117 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  // Load local NPM tasks automagically
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+     globalConfig: globalConfig,
+
+    // JS Hint
+    // =====================================================
+
+    jshint : {
+      all: [
+        'workspace/assets/js/main.js'
+      ]
+    },
+
+    // LESS
+    // =====================================================
+
+    // less: {
+    //   main: {
+    //     options: {
+    //       compress: true
+    //     },
+    //     files: {
+    //       'workspace/theme/css/main.css': 'workspace/assets/less/main.less'
+    //     }
+    //   }
+    // },
+
+    // Recess
+    // =====================================================
+
+    recess: {
+      dist: {
+        options: {
+          compile: true,
+          compress: true
+        },
+        files: {
+          'workspace/assets/css/main.css': ['workspace/assets/less/main.less']
+        }
+      }
+    },
+
+    // Concatenation
+    // =====================================================
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src : [
+          'workspace/assets/js/jquery.js',
+          'workspace/assets/bootstrap/js/combined/2.2.1/bootstrap.min.js ',
+          'workspace/assets/js/responsimage.js',
+          'workspace/assets/js/mediaelement.js',
+          'workspace/assets/js/main.js'],
+        dest: 'workspace/assets/js/application.js'
+      }
+    },
+
+    // Uglify.js
+    // =====================================================
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      main: {
+        files: {
+          'workspace/assets/js/application.min.js': ['workspace/assets/js/application.js']
+        }
+      }
+    },
+
+    // Notifications
+    // =====================================================
+
+    notify: {
+      main: {
+        options: {
+          title: '<%= globalConfig.client  %>.dev',
+          message: 'LESS and JS were compiled',
+        }
+      }
+    },
+
+    // Clean
+    // =====================================================
+
+    clean: [ "manifest/cache/*.jpg" ],
+
+    // Watch
+    // =====================================================
+
+    watch: {
+      main: {
+        files: ['**/*.less','**/*.js','!**/node_modules/**'],
+        tasks: ['core', 'notify'],
+        options: {
+          livereload: true,
+        }
+      }
+    }
+
+
+});
+
+// Main task
+grunt.registerTask('core', ['jshint', 'concat', 'uglify', 'recess']);
+grunt.registerTask('wipe', ['clean']);
+grunt.registerTask('build', ['core', 'notify']);
+grunt.registerTask('default', ['build', 'watch'])};
--- README.md
+++ README.md
@@ -18,7 +18,10 @@ On the front-end we are leveraging the following:
 - [Grunt](http://gruntjs.com/): this powerful builder helps combine and minify LESS into CSS as well as concat and minify JS files
 - [Bootstrap 2.3](http://getbootstrap.com/2.3.2/): the current version of this site is built on Bootstrap 2.3's LESS implementation, which is built via Grunt (see above)
 
+### Templates
 
+All of the XSL templates used to manipulate the `data-sources` are located in `workspace/assets/xsl`. Each template centers around a section of the site and they contain the logic used to manipulate the data passed from `Sections` to `Data Sources`.
 
+### Data Sources
 
-
+The `workspace/data-sources` directory houses all of the data that is used on the Athey Creek site. The vast majority are passed into a single page: `Home`. We rely on the `[Conditionalizer](http://symphonyextensions.com/extensions/conditionalizer/)` plugin to ensure that page loads times stay short by only loading the appropriate `Data Sources` on the needed pages.

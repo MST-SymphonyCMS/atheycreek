a3910554b43694a06523de1f2e9833401c00d08d
Jonathan Simcoe <jdsimcoe@gmail.com>
2014-08-26 16:07:21 -0700

    updates

--- workspace/data-sources/data.events_entries_by_tag_related.php
+++ workspace/data-sources/data.events_entries_by_tag_related.php
@@ -5,11 +5,14 @@ require_once TOOLKIT . '/class.datasource.php';
 class datasourceevents_entries_by_tag_related extends SectionDatasource
 {
     public $dsParamROOTELEMENT = 'events-entries-by-tag-related';
+		public $dsParamConditionalizer = '(if all of ((if value of ({$pt1}) is (events)), (if value of ({$pt4}) is (related))) is (yes))';
     public $dsParamORDER = 'desc';
     public $dsParamPAGINATERESULTS = 'yes';
     public $dsParamLIMIT = '{$pt3:5}';
     public $dsParamSTARTPAGE = '{$pt2:1}';
     public $dsParamREDIRECTONEMPTY = 'no';
+    public $dsParamREDIRECTONFORBIDDEN = 'no';
+    public $dsParamREDIRECTONREQUIRED = 'no';
     public $dsParamREQUIREDPARAM = '$pt5';
     public $dsParamSORT = 'date';
     public $dsParamHTMLENCODE = 'yes';
@@ -18,7 +21,6 @@ class datasourceevents_entries_by_tag_related extends SectionDatasource
     public $dsParamFILTERS = array(
         '35' => 'later than now',
         '137' => '{$pt5}',
-        '169' => '(if all of ((if value of ({$pt1}) is (events)), (if value of ({$pt4}) is (related))) is (yes))',
         '213' => 'no',
     );
 
@@ -40,9 +42,9 @@ class datasourceevents_entries_by_tag_related extends SectionDatasource
             'author' => array(
                 'name' => 'Jonathan Simcoe',
                 'website' => 'http://atheycreek.dev',
-                'email' => 'jonathan@simko.io'),
-            'version' => 'Symphony 2.5.0beta2',
-            'release-date' => '2014-08-04T23:42:32+00:00'
+                'email' => 'jdsimcoe@gmail.com'),
+            'version' => 'Symphony 2.5.0RC1',
+            'release-date' => '2014-08-26T20:35:24+00:00'
         );
     }
 

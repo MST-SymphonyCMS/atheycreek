8837fd7c3bb2d3cf851a07053d6e9b718a2a365e
Jonathan Simcoe <jdsimcoe@users.noreply.github.com>
2014-12-19 12:19:44 -0800

    Create data.teachings_tags_all.php

---
+++ workspace/data-sources/data.teachings_tags_all.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once TOOLKIT . '/class.datasource.php';
+
+class datasourceteachings_tags_all extends SectionDatasource
+{
+    public $dsParamROOTELEMENT = 'teachings-tags-all';
+		public $dsParamConditionalizer = '(if all of ((if value of ({$pt1}) is (teachings)), (if value of ({$pt2}) is (tag))) is (yes))';
+    public $dsParamORDER = 'asc';
+    public $dsParamPAGINATERESULTS = 'no';
+    public $dsParamLIMIT = '20';
+    public $dsParamSTARTPAGE = '1';
+    public $dsParamREDIRECTONEMPTY = 'no';
+    public $dsParamREDIRECTONFORBIDDEN = 'no';
+    public $dsParamREDIRECTONREQUIRED = 'no';
+    public $dsParamSORT = 'tag';
+    public $dsParamHTMLENCODE = 'no';
+    public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
+
+    public $dsParamFILTERS = array(
+        '217' => 'no',
+    );
+
+    public $dsParamINCLUDEDELEMENTS = array(
+        'tag: unformatted'
+    );
+
+    public function __construct($env = null, $process_params = true)
+    {
+        parent::__construct($env, $process_params);
+        $this->_dependencies = array();
+    }
+
+    public function about()
+    {
+        return array(
+            'name' => 'Teachings: Tags: All',
+            'author' => array(
+                'name' => 'Jonathan Simcoe',
+                'website' => 'http://atheycreek.dev',
+                'email' => 'jdsimcoe@gmail.com'),
+            'version' => 'Symphony 2.5.2',
+            'release-date' => '2014-12-19T20:11:22+00:00'
+        );
+    }
+
+    public function getSource()
+    {
+        return '15';
+    }
+
+    public function allowEditorToParse()
+    {
+        return true;
+    }
+
+    public function execute(array &$param_pool = null)
+    {
+        $result = new XMLElement($this->dsParamROOTELEMENT);
+
+        try{
+            $result = parent::execute($param_pool);
+        } catch (FrontendPageNotFoundException $e) {
+            // Work around. This ensures the 404 page is displayed and
+            // is not picked up by the default catch() statement below
+            FrontendPageNotFoundExceptionHandler::render($e);
+        } catch (Exception $e) {
+            $result->appendChild(new XMLElement('error', $e->getMessage() . ' on ' . $e->getLine() . ' of file ' . $e->getFile()));
+            return $result;
+        }
+
+        if ($this->_force_empty_result) {
+            $result = $this->emptyXMLSet();
+        }
+
+        if ($this->_negate_result) {
+            $result = $this->negateXMLSet();
+        }
+
+        return $result;
+    }
+}
